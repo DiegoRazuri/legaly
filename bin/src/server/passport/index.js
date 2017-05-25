5d781938f7c6ed6b57856431aa62b6d6822a98c3
The Creator <diegorazuri@gmail.com>
2017-05-25 12:29:59 -0500

    se agrego la variable de entorno de GOOGLE_CALLBACK_URI en el archivo index.js de passport

--- bin/src/server/passport/index.js
+++ bin/src/server/passport/index.js
@@ -97,7 +97,7 @@ module.exports = function (passport) {
 	passport.use(new GoogleStrategy({
 		clientID: process.env.GOOGLE_CLIENT_ID,
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-		callbackURL: "http://localhost:3000/auth/google/callback"
+		callbackURL: process.env.GOOGLE_CALLBACK_URI || "http://localhost:3000/auth/google/callback"
 
 	}, function (accessToken, refreshToken, profile, done) {
 
